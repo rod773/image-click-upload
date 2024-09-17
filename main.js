b14c3e2f167da04c8c7d1fdedcd42be38923fbea
rod <rodrigoarcadio1@gmail.com>
2024-09-17 10:39:11 +0200

    update

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ const openImgUpload = document.getElementById("OpenImgUpload");
 openImgUpload.addEventListener("click", () => {
   imgupload.click();
 });
+
+imgupload.onchange = function (e) {
+  var file = imgupload.files[0];
+  var reader = new FileReader();
+  reader.onload = function () {
+    openImgUpload.src = reader.result;
+  };
+  reader.readAsDataURL(file);
+};
