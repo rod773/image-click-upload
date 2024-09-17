8607b74c39c5392ad4e7a89c89155f0242f63835
rod <rodrigoarcadio1@gmail.com>
2024-09-17 09:53:22 +0200

    initial

---
+++ main.js
@@ -0,0 +1,7 @@
+const imgupload = document.getElementById("imgupload");
+
+const openImgUpload = document.getElementById("OpenImgUpload");
+
+openImgUpload.addEventListener("click", () => {
+  imgupload.click();
+});
